ea33a5f252ebd64c55ef136a9953450ede7ef7da
adilBayraktar <adilbayraktar1997@gmail.com>
2023-07-21 14:49:26 +0300

    baseurl updated

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const baseRequest = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://survey-api-jet.vercel.app/api",
 });
 
 export default baseRequest;
